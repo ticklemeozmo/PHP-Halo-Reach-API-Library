331d32b23c44f2dc4d35119eb2625671af572379
Ozmo Camaris <ticklemeozmo+github@gmail.com>
2010-10-08 10:04:07 -0400

    Added bungierape.php for quick caching of games locally. USE WITH CAUTION\!

---
+++ bungierape.php
@@ -0,0 +1,48 @@
+<?php
+/*
+   bungierape.php
+
+   Pre-caches game files for a gamertag.
+
+   Game files do not change once they are posted, so there is never a need to
+   redownload them.  In the event you want to import the games or use metrics
+   across many games, it's better to have them local than to grab them in an
+   unsafe manner.
+
+   NOTE: You are highly cautioned to NOT remove the sleep() command.  Bungie
+   allows 300 requests per minute on your API key and removal of that line
+   will loop as fast as your processor will allow.
+*/
+
+require_once("config.php");
+
+if (validatePlayerGamertag($_GET['gamertag'])){
+	$gamertag = $_GET['gamertag'];
+}
+else
+{
+	die("No gamertag supplied.  Rerun with bungierape.php?gamertag=SomeGamerTag");
+}
+
+$more = true;
+$count = 0;
+
+echo "Pre-caching games for $gamertag...<br/>";
+
+while($more)
+{
+	$history = getGameHistory($gamertag, "Unknown", $count);
+	foreach($history->RecentGames as $Game){
+		echo 'Downloading: ' . $Game->GameId . ' - ' . $Game->GameVariantName . ' on ' . $Game->MapName . '<br />';
+		flush_buffers();
+		$game = getGameDetails($Game->GameId);
+		sleep(1);		// THIS IS IMPORTANT! Removal of this line could ban your API key, you only get 300 requests per minute!
+	}
+	$more = $history->HasMorePages;
+	$count++;
+	if ($more) { echo "Retrieving page $count<br/>"; }
+	flush_buffers();
+}
+
+?>
+
